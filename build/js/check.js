73acf9250b396dcc703b858328a109607a89088e
SvyatoslavF <svyatoslav.fedorov@gmail.com>
2016-06-02 11:23:56 +0300

    Модуль 1 Задание 2
    Исправил код *после диалога в скайпе*.
    Синтаксис, опечатки, структура.

--- build/js/check.js
+++ build/js/check.js
@@ -1,36 +1,30 @@
-var message = "Svyat";
-var a = [1,2,3,4,5], b = [1,2,3,4,5];
+function getMessage(a, b) {
+	if (typeof a === 'boolean') {
+			if (a) {
+				return 'Я попал в ' + b;
+			}
+		return 'Я никуда не попал';
+	}
 
-function getMessage(a, b){
-	if(tipeof(a)=="boolean"){
-		if(a==true){
-			message = "Я попал в [b]";
-		};
-		else if(a==false){
-			message = "Я никуда не попал";
-		};
-	};
+	if (typeof a === 'number') {
+		return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+	}
 
-	else if(tipeof(a)=="number"){
-		message = "Я прыгнул на [a] * 100 сантиметров";
-	};
+	if (Array.isArray(a)) {
+		if (Array.isArray(b)) {
+			var distance = 0;
+				for (var i = 0; i < a.length; i++) {
+					distance = distance + a[i] * b[i];	
+				}
+			return 'Я прошёл ' + distance + ' метров';
+		}
 
-	if(Array.isArray(a) && Array.isArray(b)){
-		var distance = 0;
-		for (var i = 0; (i <= a[i].length) || (i <= b[i].length); i++) {
-			distance = distance + (a[i] * b[i]);	
-		}; 
-		message = "Я прошёл [distance] метров";
-	};
-
-	else if(Array.isArray(a)){
 		var sum = 0;
-		for (var i = 0; i <= a[i].length; i++){
-			sum = sum + a[i];
-		};
-		message = "Я прошёл [sum] шагов";
-	};
-};
-
+			for (var i = 0; i < a.length; i++) {
+				sum = sum + a[i];
+			}
+		return 'Я прошёл ' + sum + ' шагов';
+	}
+}
 
 getMessage();
